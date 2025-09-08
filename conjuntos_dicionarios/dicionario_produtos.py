01b0937f9e4a08ba8f836eadb874095aea4b5f5b
wallace.silva <gomesw@gmail.com>
2025-09-08 09:46:44 -0300

    Iniciando dicionarios

---
+++ conjuntos_dicionarios/dicionario_produtos.py
@@ -0,0 +1,12 @@
+
+dicionario_produtos = {} 
+
+for i in range(3): 
+
+    nome = input("Digite o nome do produto: ") 
+
+    quantidade = int(input("Digite a quantidade: ")) 
+
+    dicionario_produtos[nome] = quantidade 
+
+print(f"Dicionário de produtos: {dicionario_produtos}") 
---
+++ conjuntos_dicionarios/meu_dicionario.py
@@ -0,0 +1,7 @@
+dicionarios_idade_irmaos = {
+    "Wesley": 47,
+    "Wescley": 45,
+    "Wellington": 44,
+    "Wallace": 42
+}
+print(f"dicionarios_idade_irmaos: {dicionarios_idade_irmaos}")
